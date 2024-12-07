b6777fea4042f79c9e7d34cfa12f19324972d99d
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2024-12-06 23:17:14 -0500

    Fix advanced placeholders returning xyz values. add full coord placeholders

--- src/main/java/io/github/simplexdev/quickcoordscopy/QuickCoordsCopyClient.java
+++ src/main/java/io/github/simplexdev/quickcoordscopy/QuickCoordsCopyClient.java
@@ -10,6 +10,8 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 
+import java.text.DecimalFormat;
+
 public class QuickCoordsCopyClient implements ClientModInitializer {
 
     private static final KeyBinding copyCoordsKey = KeyBindingHelper
@@ -30,13 +32,20 @@ public class QuickCoordsCopyClient implements ClientModInitializer {
                 if (client.player != null) {
                     final var copyFormat = QuickCoordsCopyConfig.copyFormat;
 
+                    DecimalFormat df = new DecimalFormat("0.00");
+
                     final var coords = copyFormat
+                            .replace("$xFull", String.valueOf(df.format(client.player.getX())))
+                            .replace("$yFull", String.valueOf(df.format(client.player.getY())))
+                            .replace("$zFull", String.valueOf(df.format(client.player.getZ())))
+                            .replace("$yawSnap", String.valueOf(client.player.getFacing().getHorizontal() == 3 ? -90 : client.player.getFacing().getHorizontal() * 90))
+                            .replace("$yawFull", String.valueOf(df.format(client.player.getHeadYaw())))
+                            .replace("$yaw", String.valueOf((int) (client.player.getHeadYaw())))
+                            .replace("$pitchFull", String.valueOf(df.format(client.player.getPitch())))
+                            .replace("$pitch", String.valueOf((int) (client.player.getPitch())))
                             .replace("$x", String.valueOf((int) (client.player.getX())))
                             .replace("$y", String.valueOf((int) (client.player.getY())))
-                            .replace("$z", String.valueOf((int) (client.player.getZ())))
-                            .replace("$yaw", String.valueOf((int) (client.player.getHeadYaw())))
-                            .replace("$pitch", String.valueOf((int) (client.player.getPitch())))
-                            .replace("$yawSnap", String.valueOf(client.player.getFacing().getHorizontal() == 3 ? -90 : client.player.getFacing().getHorizontal() * 90));
+                            .replace("$z", String.valueOf((int) (client.player.getZ())));
 
                     client.keyboard.setClipboard(coords);
                     if (QuickCoordsCopyConfig.confirmation) {
